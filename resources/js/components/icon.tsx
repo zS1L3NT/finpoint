fd966fee031eeb45064ddd1a37fb95dfa001b8c6
zS1L3NT Mac <dev@zectan.com>
2026-04-14 16:42:46 +0800

    ♻️ make icon resizable

--- resources/js/components/icon.tsx
+++ resources/js/components/icon.tsx
@@ -1,14 +1,22 @@
 import { DynamicIcon, IconName, iconNames } from "lucide-react/dynamic"
 import React from "react"
 
-export default function Icon({ name, color }: { name: string; color: string }) {
+export default function Icon({
+	name,
+	color,
+	size = 20,
+}: {
+	name: string
+	color: string
+	size?: number
+}) {
 	return (
 		<div
 			className="d-flex justify-content-center align-items-center rounded-circle"
-			style={{ width: 40, height: 40, backgroundColor: color }}
+			style={{ width: size * 2, height: size * 2, backgroundColor: color }}
 		>
 			{iconNames.includes(name as IconName) ? (
-				<DynamicIcon name={name as IconName} color="white" size={20} />
+				<DynamicIcon name={name as IconName} color="white" size={size} />
 			) : null}
 		</div>
 	)
