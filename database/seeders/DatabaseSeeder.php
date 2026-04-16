5404b253356f1a6e0a9325531b53ecca52058923
zS1L3NT Mac <dev@zectan.com>
2026-04-16 10:37:21 +0800

    ✨ add category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,215 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $categories = [
+            [
+                "name" => "Electronics & Software",
+                "icon" => "cpu",
+                "color" => "#546CFE",
+                "children" => [
+                    [
+                        "name" => "Device",
+                        "icon" => "monitor-smartphone"
+                    ],
+                    [
+                        "name" => "SIM Card",
+                        "icon" => "card-sim"
+                    ],
+                    [
+                        "name" => "ESIM Card",
+                        "icon" => "card-sim"
+                    ],
+                    [
+                        "name" => "Multimedia",
+                        "icon" => "folder-code"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Other",
+                "icon" => "circle-question-mark",
+                "color" => "#9E9E9E",
+                "children" => [
+                    [
+                        "name" => "Transfer",
+                        "icon" => "arrow-left-right",
+                    ]
+                ]
+            ],
+            [
+                "name" => "Loans",
+                "icon" => "hand-coins",
+                "color" => "#01BFA5",
+                "children" => [
+                    [
+                        "name" => "Lending",
+                        "icon" => "banknote-arrow-up"
+                    ],
+                    [
+                        "name" => "Repayment",
+                        "icon" => "banknote-arrow-down"
+                    ],
+                    [
+                        "name" => "Reimbursement",
+                        "icon" => "banknote"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Gifts",
+                "icon" => "gift",
+                "color" => "#AB47BD",
+                "children" => [
+                    [
+                        "name" => "Birthday",
+                        "icon" => "cake"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Transport",
+                "icon" => "navigation",
+                "color" => "#788F9C",
+                "children" => [
+                    [
+                        "name" => "Airplane",
+                        "icon" => "plane"
+                    ],
+                    [
+                        "name" => "Bicycle",
+                        "icon" => "bike"
+                    ],
+                    [
+                        "name" => "Taxi",
+                        "icon" => "car-taxi-front"
+                    ],
+                    [
+                        "name" => "Train & Bus",
+                        "icon" => "train-front"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Health & Personal Care",
+                "icon" => "heart-pulse",
+                "color" => "#FFB300",
+                "children" => [
+                    [
+                        "name" => "Cosmetic",
+                        "icon" => "mirror-round"
+                    ],
+                    [
+                        "name" => "Haircut",
+                        "icon" => "scissors"
+                    ],
+                    [
+                        "name" => "Healthcare",
+                        "icon" => "hospital"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Clothing & Footwear",
+                "icon" => "shopping-bag",
+                "color" => "#4FC3F7",
+                "children" => [
+                    [
+                        "name" => "Accessories",
+                        "icon" => "hat-glasses"
+                    ],
+                    [
+                        "name" => "Clothes",
+                        "icon" => "shirt"
+                    ],
+                    [
+                        "name" => "Shoes",
+                        "icon" => "footprints"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Food & Drinks",
+                "icon" => "utensils",
+                "color" => "#F44336",
+                "children" => [
+                    [
+                        "name" => "Grocery",
+                        "icon" => "apple"
+                    ],
+                    [
+                        "name" => "Coffee",
+                        "icon" => "coffee"
+                    ],
+                    [
+                        "name" => "Restaurant",
+                        "icon" => "soup"
+                    ],
+                    [
+                        "name" => "Alcohol",
+                        "icon" => "wine"
+                    ],
+                    [
+                        "name" => "Dessert",
+                        "icon" => "ice-cream-bowl"
+                    ],
+                    [
+                        "name" => "Sweet Drink",
+                        "icon" => "cup-soda"
+                    ]
+                ]
+            ],
+            [
+                "name" => "Leisure & Hobby",
+                "icon" => "party-popper",
+                "color" => "#64DD17",
+                "children" => [
+                    [
+                        "name" => "Venue Rental",
+                        "icon" => "warehouse"
+                    ],
+                    [
+                        "name" => "Class",
+                        "icon" => "book-open"
+                    ],
+                    [
+                        "name" => "Concert",
+                        "icon" => "ticket"
+                    ],
+                    [
+                        "name" => "Equipment",
+                        "icon" => "toolbox"
+                    ],
+                    [
+                        "name" => "Instrument",
+                        "icon" => "guitar"
+                    ],
+                    [
+                        "name" => "Membership",
+                        "icon" => "id-card"
+                    ],
+                    [
+                        "name" => "Merchandise",
+                        "icon" => "heart"
+                    ],
+                    [
+                        "name" => "Movie",
+                        "icon" => "film"
+                    ]
+                ]
+            ]
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($categories as $category) {
+            $id = Str::slug($category["name"]);
+            Category::create(compact("id") + collect($category)->except("children")->toArray());
+
+            if (isset($category["children"])) {
+                $base = ["color" => $category["color"], "parent_category_id" => $id];
+                foreach ($category["children"] as $category) {
+                    $id = Str::slug($category["name"]);
+                    Category::create(compact("id") + $base + $category);
+                }
+            }
+        }
     }
 }
